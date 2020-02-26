5560c7b3ac4b0b8262936f8bc6f5ab8e1b46ba99
User <chalasaninagasaketh@gmail.com>
2020-02-26 19:49:51 +0530

    House_committed

---
+++ Saketh_log4_House/src/main/java/Saketh_log4_House/Saketh_log4_House/House.java
@@ -0,0 +1,32 @@
+package Saketh_log4_House.Saketh_log4_House;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class House {
+	private static final Logger Logger=LogManager.getLogger(House.class);
+	public double FullyHomeCost(String MaterialStandard,double areaofhouseinFeets)
+	{
+		Logger.info("StandardMaterials costs");
+		double cost=0;
+		if(MaterialStandard.equals("StandardMaterials"))
+		{
+			cost=1200*(areaofhouseinFeets);
+		}
+		else if(MaterialStandard.equals("above StandardMaterials "))
+			
+		{
+			cost=1500*(areaofhouseinFeets);
+		}
+		else if(MaterialStandard.equals("HighStandardMaterials"))
+		{
+			cost=1800*(areaofhouseinFeets);
+		}
+		else if(MaterialStandard.equals("HighStandardMaterialAndFullyAutomatedHome"))
+		{
+			cost=2500*(areaofhouseinFeets);
+		}
+		return cost;
+	}
+
+
+}
